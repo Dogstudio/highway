a1515cfc21b53ee898a9748ba017bb388f4d2d64
Anthony Du Pont <bulldog@dogstudio.be>
2018-03-16 15:39:58 +0100

    :sparkles: Basic CSS Transition Example

---
+++ examples/basic-css-transition/src/transitions/fade.js
@@ -0,0 +1,25 @@
+// To create your custom transition you need to provide two required methods:
+// - `in`: The transition part to show your view.
+// - `out`: The transition part to hide you view.
+//
+// Each method receives two parameters, the `view` and a callback method called
+// `done` you will always have to call when the method is over.
+const Fade = {
+  in: (view, done) => {
+    // Add a classname to trigger the animation
+    view.classList.add('fade-in');
+
+    // Wait for the animation to end
+    view.addEventListener('animationend', done);
+  },
+  out: (view, done) => {
+    // Add a classname to trigger the animation
+    view.classList.add('fade-out');
+
+    // Wait for the animation to end
+    view.addEventListener('animationend', done);
+  }
+};
+
+// Don't forget to export in some way your custom transition.
+export default Fade;
