66f20fab1b045622a3d4789f014287733f2b6445
Anthony Du Pont <bulldog@dogstudio.be>
2018-04-05 21:17:02 +0200

    :construction: Add Unit Tests

--- test/dom/home.js
+++ test/dom/home.js
@@ -20,13 +20,8 @@ const DOM = new window.DOMParser().parseFromString(HTML, 'text/html');
 // We now get the `[router-view]` from the fake DOM.
 const VIEW = DOM.querySelector('[router-view]');
 
-// Create a `Highway.Renderer` class
-class Home extends Highway.Renderer {
-  //
-}
-
-// Export the instance
-export default new Home({
+// Export instance
+export default new Highway.Renderer({
   page: DOM,
   view: VIEW,
   transition: Highway.Transition
