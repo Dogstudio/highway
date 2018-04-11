7cc2cdc3faccc2cb8263c73e95ec6d139a7f2b17
Anthony Du Pont <bulldog@dogstudio.be>
2018-04-11 15:09:15 +0200

    :construction: Add Core Unit Tests

--- test/core.tests.js
+++ test/core.tests.js
@@ -29,4 +29,21 @@ describe('Highway.Core', () => {
   it('Should be an instance of `Highway.Core`', () => {
     expect(Core).to.be.instanceof(Highway.Core);
   });
+
+  it('Should bind/unbind `click` event on links', () => {
+    const a = document.createElement('a');
+    const b = document.createElement('a');
+
+    document.body.appendChild(a);
+    document.body.appendChild(b);
+
+    sinon.spy(a, 'addEventListener');
+    sinon.spy(b, 'removeEventListener');
+
+    Core.bind();
+    Core.unbind();
+
+    expect(a.addEventListener.calledOnce).to.be.true;
+    expect(b.removeEventListener.calledOnce).to.be.true;
+  });
 });
