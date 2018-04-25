c2d8508620a63044d2e347f69d67bcb167de0725
Anthony Du Pont <bulldog@dogstudio.be>
2018-04-25 16:44:58 +0200

    :construction: Add Unit Test

--- test/core.tests.js
+++ test/core.tests.js
@@ -31,8 +31,8 @@ describe('Highway.Core', () => {
   const a = document.createElement('a');
   const b = document.createElement('a');
 
-  a.href = 'http://bar.com/foo';
-  b.href = 'http://bar.com/foo#anchor';
+  a.href = '/foo';
+  b.href = '/foo#anchor';
 
   document.body.appendChild(a);
   document.body.appendChild(b);
@@ -54,31 +54,22 @@ describe('Highway.Core', () => {
     expect(b.removeEventListener.calledOnce).to.be.true;
   });
 
-  // it('Should call `click` method on `click` event', () => {
-  //   sinon.spy(a, 'click');
-  //   sinon.spy(b, 'click');
+  it('Should call `click` method on `click` event', () => {
+    sinon.spy(a, 'click');
+    sinon.spy(b, 'click');
 
-  //   a.click();
-  //   b.click();
+    a.click();
+    b.click();
 
-  //   expect(a.click.calledOnce).to.be.true;
-  //   expect(b.click.calledOnce).to.be.true;
-  // });
+    expect(a.click.calledOnce).to.be.true;
+    expect(b.click.calledOnce).to.be.true;
+  });
 
   // it('Should call `beforeFetch` method on `popState`', () => {
   //   Core.beforeFetch = sinon.spy();
   //   Core.popState();
 
   //   expect(Core.beforeFetch.calledOnce).to.be.true;
-  // });
-
-  // it('Should fetch an URL properly', () => {
-  //   Core.state = {};
-  //   Core.state.url = 'http://foo.com/bar';
-
-  //   Core.fetch().then((response) => {
-  //     expect(response.url).to.equal('http://foo.com/bar');
-  //   });
   // });
 
   it('Should fetch an URL properly', () => {
