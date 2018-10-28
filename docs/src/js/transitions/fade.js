d7ec4490f3b1d7f71cb7838d3a4e8b67564694ad
Anthony Du Pont <antho.dpnt@gmail.com>
2018-10-28 22:46:11 +0100

    Clean Code a Bit

--- docs/src/js/transitions/fade.js
+++ docs/src/js/transitions/fade.js
@@ -7,20 +7,17 @@ import Tween from 'gsap';
 // Fade
 class Fade extends Highway.Transition {
   in(view, done) {
-    done();
-
-    console.log(view);
-
     // Animation
     Tween.fromTo(view, 0.5,
       { opacity: 0 },
-      { opacity: 1 }
+      {
+        opacity: 1,
+        onComplete: done
+      }
     );
   }
 
   out(view, done) {
-    console.log(view);
-
     // Animation
     Tween.fromTo(view, 0.5,
       { opacity: 1 },
