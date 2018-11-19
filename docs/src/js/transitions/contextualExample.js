58c4b01afbe86cf61ed372c76fac478688500c2f
Josh Kirk <josh@zero.nyc>
2018-11-19 13:04:20 -0600

    changed contextual example code to work with the correct method of overlapping view management

--- docs/src/js/transitions/contextualExample.js
+++ docs/src/js/transitions/contextualExample.js
@@ -7,19 +7,20 @@ import Tween from 'gsap';
 // Basic
 class Contextual extends Highway.Transition {
   in(view, done) {
-    // We reset the scroll position. This can happen here, earlier or later depending on your transition
-    window.scrollTo(0, 0);
-    // Done
-    done();
-
     Tween.set('.site-footer', { opacity: 1, y: 0 });
-    // Animation
-    Tween.fromTo(view, 0.7, { opacity: 0, y: 50 }, { opacity: 1, y: 0, force3D: true });
+    Tween.fromTo(view, 0.7, { autoAlpha: 0, y: 50 }, { autoAlpha: 1, y: 0, force3D: true, onComplete: () => {
+        // Done
+        done();
+      }});
   }
 
   out(view, done) {
-    // Animation
-    Tween.to([view, '.site-footer'], 0.7, { opacity: 0, y: -40, force3D: true, onComplete: done });
+    Tween.to([view, '.site-footer'], 0.7, { opacity: 0, y: -40, force3D: true, onComplete: () => {
+        Tween.set(view, { display: 'none'});
+        // We reset the scroll position. This can happen here, earlier or later depending on your transition
+        window.scrollTo(0, 0);
+        done();
+      }});
   }
 }
 
