ae7cac0a3b1ac03c8a5d1b019a4b07b9a7e4cccf
Ivo Metz <ivo@in10.nl>
2019-10-22 11:36:19 +0200

    log the renderer

--- build/highway.js
+++ build/highway.js
@@ -5994,7 +5994,7 @@ function (_Emitter) {
                   trigger: this.trigger,
                   contextual: this.Contextual
                 };
-                console.log('From', this.From); // We have to verify our cache in order to save some HTTPRequests. If we
+                console.log('From', this.From.properties.renderer); // We have to verify our cache in order to save some HTTPRequests. If we
                 // don't use any caching system everytime we would come back to a page we
                 // already saw we will have to fetch it again and it's pointless.
 
