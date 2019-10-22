92bba4441e95e7f2682bbb80c22bf508a4b8510b
Ivo Metz <ivo@in10.nl>
2019-10-22 16:02:23 +0200

    forgot classlist

--- build/highway.js
+++ build/highway.js
@@ -6040,7 +6040,7 @@ function (_Emitter) {
                 console.log('from', this.From);
 
                 if (this.From.onSleep) {
-                  this.From.wrap.firstElementChild.add('view-asleep');
+                  this.From.wrap.firstElementChild.classList.add('view-asleep');
                   console.log('check on the goto sleep do something differerent');
                 } // We have to verify our cache in order to save some HTTPRequests. If we
                 // don't use any caching system everytime we would come back to a page we
