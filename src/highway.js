e7195d5e6442ed45906ccb61187feee990818bef
Anthony Du Pont <bulldog@dogstudio.be>
2020-04-21 00:48:39 +0200

    Change Version + Add New Sites + Update Examples

--- src/highway.js
+++ src/highway.js
@@ -8,7 +8,7 @@ import Renderer from './renderer';
 import Transition from './transition';
 
 // Highway Version
-console.log('Highway v2.1.3');
+console.log('Highway v2.2.0');
 
 // Export Highway
 export default {
